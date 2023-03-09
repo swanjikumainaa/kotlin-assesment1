c234d5b33f1b4c4ff755296c707338d6cc434dcb
student <swanjikumaina@gmail.com>
2023-03-09 08:32:59 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,55 @@
+fun main(){
+
+}
+
+//1. Write and invoke a function that takes in any 2 strings and returns the first
+//character of the longer string or the first character of the first string if they are
+//equal in length. (4 pts)
+fun one(x:String,y:String):String{
+    var string1 = "x"
+    var string2 = "y"
+    when("x".length=="y".length) return "x".[0] ,"y".[0]
+    while ("x".length >= "y".length)
+        return
+
+
+
+
+}
+
+
+//2. Write and invoke one function that takes in an array of integers and returns
+//these 3 values: smallest, largest and average of all the elements (7 pts)
+fun two():Array{
+
+}
+
+
+//3. Write and call a function that takes in a string and splits it into all the
+//characters that constitute it. Your function should print out this output. (3 pts)
+fun three(){
+    var name = "Susan"
+    println("Susan".)
+}
+
+
+//4. Write and call a function that takes in an array of strings, joins them all into
+//one string and returns it. (4 pts)
+fun four (){
+
+}
+
+
+
+//5. Create a calculator class that is capable of the following functions:
+//(i) Addition of any 2 numbers
+//(ii) Subtraction between any 2 numbers
+//(iii) Accurate division between any 2 numbers
+//(iv) Multiplication of any 2 numbers
+//Instantiate your calculator object and invoke all its functions
+class Calculator(var 24:Int,var 5.6:Double):Double{
+    fun add(){
+
+    }
+
+}
